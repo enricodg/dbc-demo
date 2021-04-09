57925443b49abd64abb628689720bb38c8007ab5
enricodg <enrico.daniswaragiovanni@gmail.com>
2021-04-09 09:58:37 +0700

    build jar in Dockerfile

--- run.sh
+++ run.sh
@@ -10,26 +10,19 @@ function clean_up {
     fi
 }
 
-echo "Building jdbc-demo"
-cd jdbc-demo
-mvn clean package -DskipTests
-cd ..
-cd r2dbc-demo
-mvn clean package -DskipTests
-cd ..
+# echo "Building jdbc-demo"
+# cd jdbc-demo
+# mvn clean package -DskipTests
+# cd ..
+# cd r2dbc-demo
+# mvn clean package -DskipTests
+# cd ..
 
 echo "Starting docker"
 docker-compose build
 docker-compose up -d
 
 sleep 3
-
-# function test_service_available {
-#
-# }
-#
-# test_service_available 8000
-# test_service_available 8001
 
 trap clean_up EXIT
 
